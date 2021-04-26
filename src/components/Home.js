8629be5c9aa7864e4e33428b9faf467ebe1c8dbe
Thomas Capello <thomcapello@gmail.com>
2021-04-26 16:40:19 -0400

    redux boilerplate set up

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
+import { getClasses } from '../redux/actions'
 
-function Home() {
+function Home({ loading, dispatch }) {
     return (
         <div>
             <h1>Home</h1>
+            <button onClick={() => dispatch({type: 'GET_CLASSES'})}>Click Me</button>
+            {loading ? <h2>Loading...</h2> : <h2>Not Loading</h2>}
         </div>
     )
 }
 
-export default Home
+const mapStateToProps = (state) => {return {loading: state.loading}}
+
+export default connect(mapStateToProps)(Home)
---
+++ src/redux/actions/index.js
@@ -0,0 +1,5 @@
+export const GET_CLASSES = 'GET_CLASSES';
+
+export const getClasses = () => ({
+    type: GET_CLASSES
+})
