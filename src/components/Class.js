4977ba5c5df2fd35d4963e216cd3da37f00c724a
Austin Greer <agreer.ag12@gmail.com>
2021-04-27 14:35:47 -0400

    setup basic structur of card

--- src/components/Class.js
+++ src/components/Class.js
@@ -1,16 +1,22 @@
 import React from 'react'
 
+
 function Class(props) {
+    const { name, type, startTime, duration, intensityLevel, attendees, maxClassSize, location} = props.classes
+    
     return (
         <div>
-            <h1>{props.classes.name}</h1>
-            <p>{props.classes.type}</p>
-            <p>{props.classes.startTime}</p>
-            <p>{props.classes.duration}</p>
-            <p>{props.classes.intensityLevel}</p>
-            <p>{props.classes.location}</p>
-            <p>{props.classes.attendees}</p>
-            <p>{props.classes.maxClassSize}</p>
+            <h1>{name}</h1>
+            <ul>
+                <li>{type}</li>
+                <li>{startTime}</li>
+                <li>{duration}</li>
+                <li>{intensityLevel}</li>
+                <li>{location}</li>
+                <li>{attendees}</li>
+                <li>{maxClassSize}</li>
+            </ul>
+            
         </div>
     )
 }
