16fe0833b1cc3e783d4c21f20e588ad79a2c3a8a
Edison Jeon <edison-jeon@lambdastudents.com>
2021-04-27 14:30:15 -0700

    working through signup with group

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const initialSignUpValues = {
+    username: '',
+    password: '',
+    code: '',
+}
+
+const initialErrorValues = {
+    username: '',
+    password: '',
+    code: '',
+}
 
 function SignUp() {
+    const [signUpValues, setSetUpValues] = useState(initialSignUpValues)
+    const [errorValues, setErrorValues] = useState(initialErrorValues)
+    
+    function onChange(evt) {
+        const { name, value } = evt.target
+        setSetUpValues({
+            ...signUpValues,
+            [name]: value
+        })
+    }
+    
+    
     return (
         <div>
             <h1>Sign Up Component</h1>
+            <form className = 'form container'>
+                <label>Username:
+                    <input
+                        type = 'text'
+                        value = {signUpValues.username}
+                        onChange = {onChange}
+                        name = 'username'
+                    />
+                </label>
+                <label>Password:
+                    <input
+                        type = 'text'
+                        value = {signUpValues.password}
+                        onChange = {onChange}
+                        name = 'password'
+                    />
+                </label>
+                <label>Code:
+                    <input
+                        type = 'text'
+                        value = {signUpValues.code}
+                        onChange = {onChange}
+                        name = 'code'
+                    />
+                </label>
+                <button>Sign Up Now!</button>
+            </form>
         </div>
     )
 }
