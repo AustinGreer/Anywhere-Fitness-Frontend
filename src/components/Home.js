ef6bafee5dc2fd4bd9aa0393b7f0083bb8323485
michaelquintdev <michaelquintdev@gmail.com>
2021-04-28 11:54:06 -0500

    Done with top and body of the homepage. Only thing left is the NavBar and footer.

--- src/components/Home.js
+++ src/components/Home.js
@@ -39,18 +39,25 @@ function Home({ loading, dispatch }) {
     `
     const BodyPictureContainer = styled.div`
         display: flex;
-        justify-content: space-between;
+        justify-content: space-around;
         background-color: ${pr => pr.theme.fourthColor};
         color: white;
+        align-items:center;
     `
     const BodyTextContainer = styled.div`
-        width:45%
+        width: 70%;
+        padding: 0% 5%;
+        
     `
     const BodyPictureContainer1 = styled.div`
         display: flex;
         justify-content: space-between;
         background-color: ${pr => pr.theme.tertiaryColor};
+        align-items:center;
         color: white;
+    `
+    const Image = styled.img`
+        width: 30%;
     `
     const H1 = styled.h1`
         font-size: 3.3rem;
@@ -63,6 +70,7 @@ function Home({ loading, dispatch }) {
     `
     const H3 = styled.h3`
         font-size: 2.2rem;
+        margin-bottom: 2%;
     `
 
     useEffect(() => {
@@ -87,39 +95,33 @@ function Home({ loading, dispatch }) {
                 </TopContainer>
                 <MidContainer>
                     <H2>Below this is a big paragraph</H2>
-                    <MidParagraph>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ornare accumsan orci, eu molestie dui dignissim vitae. Donec luctus est vitae ligula eleifend, eu tincidunt magna dapibus. Maecenas non imperdiet purus. Praesent pharetra maximus interdum. Fusce vitae felis pulvinar, iaculis odio at, rhoncus augue.</MidParagraph>
+                    <MidParagraph>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna.</MidParagraph>
                 </MidContainer>
                 <BodyPictureContainer1>
                     <BodyTextContainer>
                         <H3>Sed amet aliquam</H3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ornare accumsan orci, eu molestie dui dignissim vitae. Donec luctus est vitae ligula eleifend, eu tincidunt magna dapibus. Maecenas non imperdiet purus. Praesent pharetra maximus interdum. Fusce vitae felis pulvinar, iaculis odio at, rhoncus augue.</p>
                     </BodyTextContainer>
-                    <div>
-                        <img src = {weights} alt = 'Your journey begins here'/>
-                    </div>
+                    <Image src = {weights} alt = 'Your journey begins here'/>
                 </BodyPictureContainer1>
                 <BodyPictureContainer>
-                    <div>
-                        <img src = {deadlift} alt = 'Exercise'/>
-                    </div>
-                    <div>
+                    <Image src = {deadlift} alt = 'Exercise'/>
+                    <BodyTextContainer>
                         <H3>Sed amet aliquam</H3>
                         <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
-                    </div>
+                    </BodyTextContainer>
                 </BodyPictureContainer>
                 <BodyPictureContainer1>
-                    <div>
+                    <BodyTextContainer>
                         <H3>Sed amet aliquam</H3>
                         <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
-                    </div>
-                    <div>
-                        <img src = {lifting} alt = 'Lifting'/>
-                    </div>
+                    </BodyTextContainer>
+                    <Image src = {lifting} alt = 'Lifting'/>
                 </BodyPictureContainer1>
                 <MidContainer>
                     <H2>WOAH an h2???? Again.. crazy.</H2>
                     <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet pharetra et feugiat tempus.</p>
-                    <button>Get Started</button>
+                    <TopButton>Get Started</TopButton>
                 </MidContainer>
                 <footer>
                 </footer>
