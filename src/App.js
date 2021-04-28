fdd3f726f82f82adf60376dc58b507c0310735c3
Thomas Capello <thomcapello@gmail.com>
2021-04-28 11:51:29 -0400

    added class form

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Login from './components/Login';
 import Home from './components/Home';
 import Dashboard from './components/Dashboard';
 import NavBar from './components/NavBar';
-
+import ClassForm from './components/ClassForm';
 function App() {
   return (
     <div>
@@ -15,6 +15,7 @@ function App() {
         <Route path='/signup' component={SignUp} />
         <Route path='/login' component={Login} />
         <Route path='/dashboard' component={Dashboard} />
+        <Route path='/classform' component={ClassForm} />
         <Route exact path='/' component={Home} />
       </Switch>
     </div>
--- src/components/ClassForm.js
+++ src/components/ClassForm.js
@@ -1,9 +1,111 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const initialValues = {
+    name: "",
+    type: "",
+    startTime: " ",
+    duration: " ",
+    intensityLevel: "",
+    location: "",
+    attendees: "",
+    maxClassSize: ""
+}
+
+// const initialErrValues = {
+//     name: "",
+//     type: "",
+//     startTime: " ",
+//     duration: " ",
+//     intensityLevel: "",
+//     location: "",
+//     attendees: "",
+//     maxClassSize: ""
+// }
 
 function ClassForm() {
+
+        const [values, setValues] = useState(initialValues)
+        // const [errorValues, setErrorValues] = useState(initialErrValues)
+
+        const onChange = (event) => {
+            const { name, value } = event.target;
+            setValues({ ...values, [name]: value })
+        }
+
     return (
         <div>
-
+                        <h1>Class Form</h1>
+            {/* <div className="errors">
+                            <div>{errorValues.username}</div>
+                            <div>{errorValues.password}</div>
+                        </div> */}
+            <form className = 'form container'>
+                <label>Name:
+                    <input
+                        type = 'text'
+                        value = {values.name}
+                        onChange = {onChange}
+                        name = 'name'
+                    />
+                </label>
+                <label>Type:
+                    <input
+                        type = 'text'
+                        value = {values.type}
+                        onChange = {onChange}
+                        name = 'type'
+                    />
+                </label>
+                <label>Start Time:
+                    <input
+                        type = 'text'
+                        value = {values.startTime}
+                        onChange = {onChange}
+                        name = 'startTime'
+                    />
+                </label>
+                <label>Duration:
+                    <input
+                        type = 'text'
+                        value = {values.duration}
+                        onChange = {onChange}
+                        name = 'duration'
+                    />
+                </label>
+                <label>Intensity Level:
+                    <input
+                        type = 'text'
+                        value = {values.intensityLevel}
+                        onChange = {onChange}
+                        name = 'intensityLevel'
+                    />
+                </label>
+                <label>Location:
+                    <input
+                        type = 'text'
+                        value = {values.location}
+                        onChange = {onChange}
+                        name = 'location'
+                    />
+                </label>
+                <label>Attendees:
+                    <input
+                        type = 'text'
+                        value = {values.attendees}
+                        onChange = {onChange}
+                        name = 'attendees'
+                    />
+                </label>
+                <label>Max Class Size:
+                    <input
+                        type = 'text'
+                        value = {values.maxClassSize}
+                        onChange = {onChange}
+                        name = 'maxClassSize'
+                    />
+                </label>
+                <button>Sign Up Now!</button>
+            </form>
         </div>
     )
 }
