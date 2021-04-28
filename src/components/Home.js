fd0c9d3ac69f14bc6a7e05b09f87b095c91e68be
michaelquintdev <michaelquintdev@gmail.com>
2021-04-28 12:23:02 -0500

    Done with homepage aside from footer

--- src/components/Home.js
+++ src/components/Home.js
@@ -119,7 +119,7 @@ function Home({ loading, dispatch }) {
                     <Image src = {lifting} alt = 'Lifting'/>
                 </BodyPictureContainer1>
                 <MidContainer>
-                    <H2>WOAH an h2???? Again.. crazy.</H2>
+                    <H2>WOAH an h2???? Again?.. crazy.</H2>
                     <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet pharetra et feugiat tempus.</p>
                     <TopButton>Get Started</TopButton>
                 </MidContainer>
