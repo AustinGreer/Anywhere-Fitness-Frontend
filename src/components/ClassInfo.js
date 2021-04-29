0d2f784c26e493d68781868c7f6b06516cd21191
Austin Greer <agreer.ag12@gmail.com>
2021-04-29 12:19:20 -0400

    implemented some styling in ClassInfo component with styled components

--- src/components/ClassInfo.js
+++ src/components/ClassInfo.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
 import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
 import { connect } from 'react-redux';
 import { getClassInfo } from '../redux';
 import deadlift from '../Images/deadlift.jpg';
@@ -15,9 +15,9 @@ function ClassInfo(props) {
     }, [])
 
     return (
-        <div>
+        <StyledContainer>
             {props.loading ? <h1>Loading Class Info...</h1> : ''}
-            {class_img ? <img src={class_img} alt={class_type} /> : <img src={deadlift} alt='default img' />}
+            {class_img ? <StyledImg src={class_img} alt={class_type} /> : <StyledImg src={deadlift} alt='default img' />}
             <div>
                 <h3>Type: {class_type}</h3>
                 <h3>Duration: {duration}</h3>
@@ -27,9 +27,30 @@ function ClassInfo(props) {
                 <h3>Current Attendees: {num_of_attendees}</h3>
                 <h3>Start Time: {start_time}</h3>
             </div>
-        </div>
+        </StyledContainer>
     )
 }
+
+const StyledContainer = styled.div`
+    border: 0.1rem solid white;
+    border-radius: 0.5rem;
+    width: 90%;
+    margin: 5% auto;
+    display: flex;
+    justify-content: flex-start;
+
+    h3 {
+        font-size: 3rem;
+        margin-bottom: 5%;
+        margin-right: 5%;
+    }
+`
+
+const StyledImg = styled.img`
+    width: 50%;
+    margin-right: 5%;
+    border-radius: 0.5rem;
+`
 
 const mapStateToProps = state => {
     return {
